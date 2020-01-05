220743b4819d7aa54935eb32484327a9c90d5958
cffycls <cffycls@foxmail.com>
2020-01-05 13:21:26 +0800

    添加https

--- waf.sh
+++ waf.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 docker stop n3 && docker rm n3 
-docker run -itd --name n3 -p 8084:80 --network=mybridge \
+docker run -itd --name n3 -p 8084:80 -p 8085:443 -p 9500:9500 --network=mybridge \
 	--privileged=true \
 	-v /home/wwwroot/cluster/ngx_lua_waf/conf:/usr/local/openresty/nginx/conf \
 	-v /home/wwwroot/cluster/ngx_lua_waf/logs:/usr/local/openresty/nginx/logs \
