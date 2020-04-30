027cbcdb4717b3c69917f203808c02b71b793c09
cffycls <cffycls@foxmail.com>
2020-04-30 22:16:33 +0800

    完善phpmyadmin初始化测试

--- waf.sh
+++ waf.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
-docker stop n3 && docker rm n3 
+# 8084：http1.0/1.1；8085：https；9500：代理到本机备用
+docker stop n3 && rm -rf logs/*.log && docker rm n3
 docker run -itd --name n3 -p 8084:80 -p 8085:443 -p 9500:9500 --network=mybridge \
 	--privileged=true \
+	-v /etc/timezone:/etc/localtime \
 	-v /home/wwwroot/cluster/ngx_lua_waf/conf:/usr/local/openresty/nginx/conf \
 	-v /home/wwwroot/cluster/ngx_lua_waf/logs:/usr/local/openresty/nginx/logs \
-	-v /home/wwwroot/cluster/html:/usr/local/openresty/nginx/html openresty/openresty:stretch
+	-v /home/wwwroot/cluster/html:/usr/local/openresty/nginx/html \
+	openresty/openresty:stretch
